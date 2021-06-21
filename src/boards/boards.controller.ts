fc028b2ab7167408edbca3487163e3649b342d06
lsh <fantasyz7@naver.com>
2021-06-21 16:23:53 +0900

    validation

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
@@ -20,6 +22,7 @@ export class BoardsController {
     return this.boardsService.getAllBoards();
   }
   @Post()
+  @UsePipes(ValidationPipe)
   createBoard(@Body() dto: CreateBoardDto): Board {
     //body
     // createBoard(@Body() body): Board {
--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateBoardDto {
-  @IsNotEmpty
+  @IsNotEmpty()
   title: string;
-
+  @IsNotEmpty()
   description: string;
 }

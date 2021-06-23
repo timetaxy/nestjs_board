df69f9240fd927ed9c5d84b4778d6bc44f0ddce8
lsh <fantasyz7@naver.com>
2021-06-23 17:18:02 +0900

    entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './config/typeorm.config';
 

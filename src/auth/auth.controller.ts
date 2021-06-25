6b576163b579e2b7d3c8db2f81a34c46dc45b8e2
lsh <fantasyz7@naver.com>
2021-06-25 23:47:30 +0900

    entity unique check

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential';
 
